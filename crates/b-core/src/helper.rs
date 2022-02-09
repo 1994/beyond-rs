7910b0ecbcbe43bdb7c59965c4b81d7df32a1157
1994 <reeco@vip.qq.com>
2022-02-09 23:16:08 +0800

    optimize

--- crates/b-core/src/helper.rs
+++ crates/b-core/src/helper.rs
@@ -1,7 +1,9 @@
-use std::path::{Path, PathBuf};
-
 use anyhow::Result;
 use dirs::home_dir;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 pub fn support_platform<'a>() -> Result<&'a str> {
     if cfg!(unix) || cfg!(windows) {
@@ -10,23 +12,45 @@ pub fn support_platform<'a>() -> Result<&'a str> {
         Err(anyhow::anyhow!("unsupported platform"))
     }
 }
-pub fn user_home () -> PathBuf {
+pub fn user_home() -> PathBuf {
     match home_dir() {
-        Some(p) => {
-           p
-        },
+        Some(p) => p,
         None => {
             panic!("can't get user homes")
-        },
+        }
+    }
+}
+
+pub fn backup_file(path: &str) -> anyhow::Result<()> {
+    let p = Path::new(path);
+    if !p.exists() || !p.is_file() {
+        Err(anyhow::anyhow!(format!("{} is not correct", path)))
+    } else {
+        let s = chrono::Utc::now().format("%Y%m%d%H%M").to_string();
+        let np = format!(
+            "{}.{}.bak",
+            p.file_name().unwrap().to_str().unwrap(),
+            s,
+        );
+        let p = Path::new(path);
+        let new_path = p.parent().unwrap().to_path_buf().join(np);
+        
+        fs::copy(p, new_path)?;
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
+    use crate::maven;
+
 
     #[test]
     fn test_home() {
-        let u = super::user_home();
-        println!("{:?}", u);
+        let m = maven::get_maven_settings();
+        let p = Path::new(m.as_str()).parent().unwrap().to_str().unwrap();
+        println!("{}", p);
     }
 }
--- crates/b-core/src/maven.rs
+++ crates/b-core/src/maven.rs
@@ -1,10 +1,48 @@
-use std::fs::OpenOptions;
+use std::fs::{self};
+use std::path::Path;
 
 use crate::helper;
 use crate::optimize::Action;
 struct MavenOp {}
+
+const ALIYUN_MIRROR: &str = r#"
+        <mirror>
+   		  <id>aliyunmaven</id>
+    	  <mirrorOf>*</mirrorOf>
+          <name>阿里云公共仓库</name>
+    	  <url>https://maven.aliyun.com/repository/public</url>
+		</mirror>
+"#;
+
+const BLANK: &str = r###"
+<?xml version="1.0" encoding="UTF-8"?>
+<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+    <interactiveMode>true</interactiveMode>
+    <usePluginRegistry>false</usePluginRegistry>
+    <offline>false</offline>
+    <mirrors>
+     	<mirror>
+   		  <id>aliyunmaven</id>
+    	  <mirrorOf>*</mirrorOf>
+          <name>阿里云公共仓库</name>
+    	  <url>https://maven.aliyun.com/repository/public</url>
+		</mirror>
+    </mirrors>
+</settings>
+"###;
+
 impl Action for MavenOp {
     fn execute(&self) -> anyhow::Result<()> {
+        let maven = get_maven_settings();
+        if !Path::new(maven.as_str()).exists() {
+            fs::write(maven, BLANK)?;
+        } else {
+            helper::backup_file(&maven)?;
+            todo!()
+        }
+
         Ok(())
     }
 
@@ -13,11 +51,20 @@ impl Action for MavenOp {
     }
 }
 
-fn get_maven_settings() -> String {
+pub fn get_maven_settings() -> String {
     let mut p = helper::user_home();
     p.push(".m2");
     p.push("settings.xml");
-
     p.into_os_string().into_string().unwrap()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_maven() {
+        let m = MavenOp {};
+        m.execute();
+    }
+}
